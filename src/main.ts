f1daae8ef9886ab308ef76badb866b04b3d81b07
Dev Son <devson2561@gmail.com>
2023-05-06 00:21:14 +0700

    Config Startup, Swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,37 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  app.enableCors({
+    origin: '*',
+  });
+
+  app.useGlobalPipes(new ValidationPipe());
+
+  // swagger
+  const config = new DocumentBuilder()
+    .setTitle('Walking Rabbit API')
+    .setDescription('The Walking Rabbit API description')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+  // end - swagger
+
+  const PORT = process.env.SERVER_PORT;
+
+  try {
+    await app.listen(PORT);
+    console.log(`APP IS RUNNING ON PORT ${PORT}`);
+    console.log(`docs: http://localhost:${PORT}/docs`);
+  } catch (error) {
+    console.log(error, '<--- error');
+  }
 }
 bootstrap();
